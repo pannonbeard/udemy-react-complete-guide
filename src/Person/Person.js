39f654980565a9ab696920f6bccb1cea1037d99f
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 15:56:28 -0500

    commented out radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,17 @@
 import React from 'react'
+//import Radium from 'radium'
 import './Person.css'
 
-const person = ({name, age, click, change, children}) =>
-  <div className="person">
-    <p onClick={click}>I'm a {name} who is {age} years old</p>
-    {children}<br />
-    <input type="text" onChange={change} value={name}/>
-  </div>
+const person = ({name, age, click, change, children}) =>{
+  return(
+    <div className="person">
+      <p onClick={click}>I'm a {name} who is {age} years old</p>
+      {children}<br />
+      <input type="text" onChange={change} value={name}/>
+    </div>
+  )
+}
+  
 
+//export default Radium(person);
 export default person;
