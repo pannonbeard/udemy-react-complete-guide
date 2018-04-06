2d458a506b0e1aaab086aa3a11f2d0a696a6fe6d
Phil Hendricks <pfhendri1@gmail.com>
2018-04-06 11:34:58 -0500

    added throwing error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from './Person.css'
 
 const person = ({name, age, click, change, children}) =>{
+  const rnd = Math.random();
+  if( rnd > 0.7 ){
+    throw new Error( 'Something went wrong' )
+  }
   return(
     <div className={styles.person}>
       <p onClick={click}>I'm a {name} who is {age} years old</p>
